d38a27a2365981ebf202fe91154a33ad982c7708
NguyenNhatHao107641 <hao107641@donga.edu.vn>
2024-10-30 17:22:08 +0700

    Update bai3.java

--- Buổi4/bai3.java
+++ Buổi4/bai3.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+public class bai3
+{
+    public static void soLe(int n)
+    {
+        System.out.printf("Cac so le tu 1 den %d la: ",n);
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==1)
+            {
+                System.out.printf("\t"+ i);
+
+            }
+        }
+    }
+    public static void main(String[] args) 
+    {
+        Scanner nhap = new Scanner(System.in);
+        System.out.printf("Nhap gia tri n: ");
+        int num = nhap.nextInt();
+        soLe(num);        
+    }
+}
 
