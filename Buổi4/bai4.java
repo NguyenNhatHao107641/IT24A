ae6fb59d8632736edf6e41e6b6a724394d6de5ce
NguyenNhatHao107641 <hao107641@donga.edu.vn>
2024-10-30 17:25:10 +0700

    Update bai4.java

--- Buổi4/bai4.java
+++ Buổi4/bai4.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+public class bai4
+{
+    public static void soChan(int n)
+    {
+        System.out.printf("Cac so chan tu 1 den %d la: ",n);
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.printf("\t"+ i);
+
+            }
+        }
+    }
+    public static void main(String[] args) 
+    {
+        Scanner nhap = new Scanner(System.in);
+        System.out.printf("Nhap gia tri n: ");
+        int num = nhap.nextInt();
+        soChan(num);
+    }
+}
 
