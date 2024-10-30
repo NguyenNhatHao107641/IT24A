5af254171cfb3cdd159afab4c6a0c4cfa86180cb
NguyenNhatHao107641 <hao107641@donga.edu.vn>
2024-10-30 17:14:11 +0700

    Update bai2.java

--- Buổi4/bai2.java
+++ Buổi4/bai2.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+public class bai2 {
+    public static void bai2Dem(int n){
+        System.out.printf("Cac so tu 1 den %d la",n);
+        for(int i=1;i<=n;i++){
+            System.out.printf("\t"+ i);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner nhap = new Scanner(System.in);
+        System.out.printf("Nhap gia tri n: ");
+        int num = nhap.nextInt();
+        bai2Dem(num);
+        
+    }
+}
 
