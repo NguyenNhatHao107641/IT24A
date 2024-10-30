4fba6627eb64f39dfcc91a2181211f4040792ab7
NguyenNhatHao107641 <hao107641@donga.edu.vn>
2024-10-30 17:32:34 +0700

    Update bai5.java

--- Buổi4/bai5.java
+++ Buổi4/bai5.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
+public class bai5
+{
+    public static void soLe(int n)
+    {
+        System.out.printf("Cac so le nho hon so %d la: ",n);
+        for(int i=1;i<n;i++)
+        {
+            if(i%2==1)
+            {
+                System.out.printf("\t"+ i);
 
+            }
+        }
+    }
+    public static void main(String[] args) 
+    {
+        Scanner nhap = new Scanner(System.in);
+        System.out.printf("Nhap gia tri n: ");
+        int num = nhap.nextInt();
+        soLe(num);        
+    }
+}
