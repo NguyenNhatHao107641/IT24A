934cee3e9f13344e7d5ef8d5f56cd775058fd07b
NguyenNhatHao107641 <hao107641@donga.edu.vn>
2024-10-31 00:24:24 +0700

    Update and rename bai1.java to Bai1.java

---
+++ Buoi6/Bai1.java
@@ -0,0 +1,32 @@
+public class bai1 
+{
+    public static void tH1()
+    {
+        String hoTen = "Nguyễn Văn An";
+        String ngaySinh = "20/10/2006";
+        String diemTongKet = "5.5";
+        String tuoi = "18";
+        System.out.printf("-----THÔNG TIN SINH VIÊN-----\n");
+        System.out.printf("Ho va ten : %s \n",hoTen);
+        System.out.printf("Ngay sinh : %s\n",ngaySinh);
+        System.out.printf("Diem tong ket : %s \n",Float.parseFloat(diemTongKet));
+        System.out.printf("Tuoi : %s \n",Integer.parseInt(tuoi));
+    }
+    public static void tH2()
+    {
+        String hoTen = "Nguyễn Văn An";
+        String ngaySinh = "20/10/2006";
+        Float diemTongKet = 5.5f;
+        int tuoi = 18;
+        System.out.printf("-----THÔNG TIN SINH VIÊN-----\n");
+        System.out.printf("Ho va ten : %s \n",hoTen);
+        System.out.printf("Ngay sinh : %s\n",ngaySinh);
+        System.out.printf("Diem tong ket : %s \n",Float.toString(diemTongKet));
+        System.out.printf("Tuoi : %s \n",Integer.toString(tuoi));
+    }
+    public static void main(String[] args) {
+        tH1();
+        tH2();
+    }
+}
+
