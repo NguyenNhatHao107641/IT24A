a38b02bdf1b3b4498b216f4725a64fea2a6a62cd
NguyenNhatHao107641 <hao107641@donga.edu.vn>
2024-10-30 17:35:14 +0700

    Update bai6.java

--- Buổi4/bai6.java
+++ Buổi4/bai6.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
+public class bai6
+{
+    public static void soChan(int n)
+    {
+        System.out.printf("Cac so chan nho hon so %d la: ",n);
+        for(int i=1;i<n;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.printf("\t"+ i);
 
+            }
+        }
+    }
+    public static void main(String[] args) 
+    {
+        Scanner nhap = new Scanner(System.in);
+        System.out.printf("Nhap gia tri n: ");
+        int num = nhap.nextInt();
+        soChan(num);        
+    }
+}
